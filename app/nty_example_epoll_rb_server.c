cc21dda18476be93546c9174717750db85027d9e
deepke <deepke@deepke.cn>
2020-01-05 21:53:31 +0800

    修改实例代码include的头文件名字

--- app/nty_example_epoll_rb_server.c
+++ app/nty_example_epoll_rb_server.c
@@ -57,7 +57,7 @@
 
 #include "nty_api.h"
 
-#include "nty_epoll.h"
+#include "dk_epoll.h"
 
 #include <stdint.h>
 #include <stdio.h>
@@ -77,114 +77,133 @@
 #err "Open NTY_ENABLE_EPOLL_RB"
 #endif
 
-#define EPOLL_SIZE		5
-#define BUFFER_LENGTH	1024
+#define EPOLL_SIZE      5
+#define BUFFER_LENGTH   1024
 
-int main() {
+int main()
+{
 
-	nty_tcp_setup();
+    nty_tcp_setup();
 
-	usleep(1);
+    usleep ( 1 );
 
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		perror("socket");
-		return 1;
-	}
+    int sockfd = socket ( AF_INET, SOCK_STREAM, 0 );
+    if ( sockfd < 0 )
+    {
+        perror ( "socket" );
+        return 1;
+    }
 
-	struct sockaddr_in addr;
-	memset(&addr, 0, sizeof(struct sockaddr_in));
+    struct sockaddr_in addr;
+    memset ( &addr, 0, sizeof ( struct sockaddr_in ) );
 
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(9096);
-	addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons ( 9096 );
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-	if(bind(sockfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) < 0) {
-		perror("bind");
-		return 2;
-	}
+    if ( bind ( sockfd, ( struct sockaddr* ) &addr, sizeof ( struct sockaddr_in ) ) < 0 )
+    {
+        perror ( "bind" );
+        return 2;
+    }
 
-	if (listen(sockfd, 5) < 0) {
-		return 3;
-	}
+    if ( listen ( sockfd, 5 ) < 0 )
+    {
+        return 3;
+    }
 
-	int epoll_fd = epoll_create(EPOLL_SIZE);
-	struct epoll_event ev, events[EPOLL_SIZE];
+    int epoll_fd = epoll_create ( EPOLL_SIZE );
+    struct epoll_event ev, events[EPOLL_SIZE];
 
-	ev.events = NTY_EPOLLIN;
-	ev.data.fd = sockfd;
-	epoll_ctl(epoll_fd, NTY_EPOLL_CTL_ADD, sockfd, &ev);
+    ev.events = NTY_EPOLLIN;
+    ev.data.fd = sockfd;
+    epoll_ctl ( epoll_fd, NTY_EPOLL_CTL_ADD, sockfd, &ev );
 
-	while (1) {
+    while ( 1 )
+    {
 
-		int nready = epoll_wait(epoll_fd, events, EPOLL_SIZE, -1);
-		if (nready == -1) {
-			printf("epoll_wait\n");
-			break;
-		} 
+        int nready = epoll_wait ( epoll_fd, events, EPOLL_SIZE, -1 );
+        if ( nready == -1 )
+        {
+            printf ( "epoll_wait\n" );
+            break;
+        }
 
-		printf("nready : %d\n", nready);
+        printf ( "nready : %d\n", nready );
 
-		int i = 0;
-		for (i = 0;i < nready;i ++) {
+        int i = 0;
+        for ( i = 0; i < nready; i ++ )
+        {
 
-			if (events[i].data.fd == sockfd) {
+            if ( events[i].data.fd == sockfd )
+            {
 
-				struct sockaddr_in client_addr;
-				memset(&client_addr, 0, sizeof(struct sockaddr_in));
-				socklen_t client_len = sizeof(client_addr);
-			
-				int clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
-				if (clientfd <= 0) continue;
-	
-				char str[INET_ADDRSTRLEN] = {0};
-				printf("recvived from %s at port %d, sockfd:%d, clientfd:%d\n", inet_ntop(AF_INET, &client_addr.sin_addr, str, sizeof(str)),
-					ntohs(client_addr.sin_port), sockfd, clientfd);
+                struct sockaddr_in client_addr;
+                memset ( &client_addr, 0, sizeof ( struct sockaddr_in ) );
+                socklen_t client_len = sizeof ( client_addr );
 
-				ev.events = NTY_EPOLLIN | NTY_EPOLLET;
-				ev.data.fd = clientfd;
-				epoll_ctl(epoll_fd, NTY_EPOLL_CTL_ADD, clientfd, &ev); 
-				
-			} else {
+                int clientfd = accept ( sockfd, ( struct sockaddr* ) &client_addr, &client_len );
+                if ( clientfd <= 0 )
+                {
+                    continue;
+                }
 
-				int clientfd = events[i].data.fd;
+                char str[INET_ADDRSTRLEN] = {0};
+                printf ( "recvived from %s at port %d, sockfd:%d, clientfd:%d\n", inet_ntop ( AF_INET, &client_addr.sin_addr, str, sizeof ( str ) ),
+                         ntohs ( client_addr.sin_port ), sockfd, clientfd );
 
-				char buffer[BUFFER_LENGTH] = {0};
-				int ret = recv(clientfd, buffer, BUFFER_LENGTH, 0);
-				if (ret < 0) {
-					if (errno == EAGAIN || errno == EWOULDBLOCK) {
-						printf("read all data");
-					}
-					
-					ev.events = NTY_EPOLLIN | NTY_EPOLLET;
-					ev.data.fd = clientfd;
-					epoll_ctl(epoll_fd, NTY_EPOLL_CTL_DEL, clientfd, &ev);
-					
-					close(clientfd);
-				} else if (ret == 0) {
-					printf(" disconnect %d\n", clientfd);
-					
-					ev.events = NTY_EPOLLIN | NTY_EPOLLET;
-					ev.data.fd = clientfd;
-					epoll_ctl(epoll_fd, NTY_EPOLL_CTL_DEL, clientfd, &ev);
+                ev.events = NTY_EPOLLIN | NTY_EPOLLET;
+                ev.data.fd = clientfd;
+                epoll_ctl ( epoll_fd, NTY_EPOLL_CTL_ADD, clientfd, &ev );
 
-					close(clientfd);
+            }
+            else
+            {
 
-					printf(" delete clientfd %d\n", clientfd);
-					
-					break;
-				} else {
-					printf("Recv: %s, %d Bytes\n", buffer, ret);
-					send(clientfd, buffer, ret, 0);
-				}
+                int clientfd = events[i].data.fd;
 
-			}
-		
-		}
+                char buffer[BUFFER_LENGTH] = {0};
+                int ret = recv ( clientfd, buffer, BUFFER_LENGTH, 0 );
+                if ( ret < 0 )
+                {
+                    if ( errno == EAGAIN || errno == EWOULDBLOCK )
+                    {
+                        printf ( "read all data" );
+                    }
 
-	}
+                    ev.events = NTY_EPOLLIN | NTY_EPOLLET;
+                    ev.data.fd = clientfd;
+                    epoll_ctl ( epoll_fd, NTY_EPOLL_CTL_DEL, clientfd, &ev );
 
-	return 0;
+                    close ( clientfd );
+                }
+                else if ( ret == 0 )
+                {
+                    printf ( " disconnect %d\n", clientfd );
+
+                    ev.events = NTY_EPOLLIN | NTY_EPOLLET;
+                    ev.data.fd = clientfd;
+                    epoll_ctl ( epoll_fd, NTY_EPOLL_CTL_DEL, clientfd, &ev );
+
+                    close ( clientfd );
+
+                    printf ( " delete clientfd %d\n", clientfd );
+
+                    break;
+                }
+                else
+                {
+                    printf ( "Recv: %s, %d Bytes\n", buffer, ret );
+                    send ( clientfd, buffer, ret, 0 );
+                }
+
+            }
+
+        }
+
+    }
+
+    return 0;
 }
 
 
